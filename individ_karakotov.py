c2554d2758e6924a95a5dfee4c218ac2578d440f
karrrakotov <karrrakotov@mail.ru>
2021-02-14 20:52:45 +0300

    Исправил ошибки и импортировал файлы

--- individ_karakotov.py
+++ individ_karakotov.py
@@ -4,7 +4,8 @@
 from datetime import date
 import sys
 import logging
-from mymodulefirst import *
+import mymodulefirst
+
 
 # Вариант 12.
 # Использовать словарь, содержащий следующие ключи: фамилия, имя; номер телефона; дата рождения (список из трех чисел).
@@ -17,111 +18,6 @@ from mymodulefirst import *
 # Выполнить индивидуальное задание, добавив возможность работы с исключениями и логгирование.
 
 
-# class IllegalMarksError(Exception):
-#
-#     def __init__(self, birthday, message="Illegal year number"):
-#         self.birthday = birthday
-#         self.message = message
-#         super(IllegalMarksError, self).__init__(message)
-#
-#     def __str__(self):
-#         return f"{self.birthday} -> {self.message}"
-#
-#
-# # Класс пользовательского исключения в случае, если введенная
-# # команда является недопустимой.
-# class UnknownCommandError(Exception):
-#
-#     def __init__(self, command, message="Неизвестная команда."):
-#         self.command = command
-#         self.message = message
-#         super(UnknownCommandError, self).__init__(message)
-#
-#     def __str__(self):
-#         return f"{self.command} -> {self.message}"
-#
-#
-# @dataclass(frozen=True)
-# class People:
-#     name: str
-#     phone_number: int
-#     birthday: list[int]
-#
-#
-# @dataclass
-# class Man:
-#     soul: List[People] = field(default_factory=lambda: [])
-#
-#     def add(self, name, phone_number, birthday):
-#         self.soul.append(
-#             People(
-#                 name=name,
-#                 phone_number=phone_number,
-#                 birthday=birthday
-#             )
-#         )
-#         self.soul.sort(key=lambda people: people.name)
-#
-#     def __str__(self):
-#         # Заголовок таблицы.
-#         table = []
-#         line = '+-{}-+-{}-+-{}-+-{}-+-{}-+-{}-+'.format(
-#             '-' * 4,
-#             '-' * 30,
-#             '-' * 20,
-#             '-' * 8,
-#             '-' * 8,
-#             '-' * 8,
-#             '-' * 8,
-#             '-' * 8,
-#             '-' * 11
-#         )
-#         table.append(line)
-#         table.append(
-#             '| {:^4} | {:^30} | {:^20} | {:^8} | {:^8} | {:^8} |'.format(
-#                 "No",
-#                 "Ф.И.",
-#                 "Номер телефона",
-#                 "День",
-#                 "Месяц",
-#                 "Год"
-#             )
-#         )
-#         table.append(line)
-#
-#         # Вывести данные о всех оценках ученика.
-#         for idx, people in enumerate(self.soul, 1):
-#             table.append(
-#                 '| {:>4} | {:<30} | {:<20} | {:>8} | {:>8} | {:>8} |'.format(
-#                     idx,
-#                     people.name,
-#                     people.phone_number,
-#                     people.birthday[0],
-#                     people.birthday[1],
-#                     people.birthday[2],
-#                 )
-#             )
-#         table.append(line)
-#
-#         return '\n'.join(table)
-#
-#     def __repr__(self):
-#         return self.__str__()
-#
-#     def select(self, period):
-#         # Проверить сведения людей из списка.
-#         parts = command.split(' ', maxsplit=2)
-#         period = int(parts[1])
-#         result = []
-#         count = 0
-#         for people in self.soul:
-#             if birthday:
-#                 if birthday[1] == period:
-#                     count += 1
-#                     result.append(people)
-#         return result
-
-
 if __name__ == '__main__':
     # Выполнить настройку логгера.
     logging.basicConfig(
@@ -129,7 +25,7 @@ if __name__ == '__main__':
         level=logging.INFO
     )
     # Список людей.
-    man = Man()
+    man = mymodulefirst.Man()
 
     # Организовать бесконечный цикл запроса команд.
     while True:
@@ -214,7 +110,7 @@ if __name__ == '__main__':
                 print("help - отобразить справку;")
                 print("exit - завершить работу с программой.")
             else:
-                raise UnknownCommandError(command)
+                raise mymodulefirst.UnknownCommandError(command)
         except Exception as exc:
             logging.error(f"Ошибка: {exc}")
             print(exc, file=sys.stderr)
